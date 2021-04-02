81fabdd8f233991a7607c7308a4dbb768db1c987
Yoni <yoniakabecky@gmail.com>
2021-04-02 08:42:55 -0700

    fix #1

--- src/components/Meta.jsx
+++ src/components/Meta.jsx
@@ -3,8 +3,11 @@ import { Helmet } from "react-helmet";
 
 export default function Meta() {
   return (
-    <Helmet>
-      <meta charset="UTF-8" />
+    <Helmet
+      htmlAttributes={{
+        lang: "en",
+      }}
+    >
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
         rel="icon"
